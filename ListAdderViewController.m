3b8829ba2a50b50f17bec3a6018fa5bc8858fbdd
lev012 <lev012@icloud.com>
2018-01-31 13:42:02 +0800

    test 8

--- ListAdderViewController.m
+++ ListAdderViewController.m
@@ -833,6 +833,22 @@ enum {
     [self recalculateTotal];
 }
 
+- (IBAction)macbook1:(id)sender
+// Called when the user taps the left bar button ("Defaults" or "Minimum").
+// If we have lots of numbers, set the list to contain a sigle entry.  If we have
+{
+#pragma unused(sender)
+    if ([self.numbers count] > 1) {
+        [self.numbers removeAllObjects];
+        [self.numbers addObject:@41];
+    } else {
+        [self.numbers replaceObjectsInRange:NSMakeRange(0, [self.numbers count]) withObjectsFromArray:[[self class] defaultNumbers]];
+    }
+    [self syncLeftBarButtonTitle];
+    
+    [self recalculateTotal];
+}
+
 - (IBAction)defaultsOrMinimumAction4343534:(id)sender
 // Called when the user taps the left bar button ("Defaults" or "Minimum").
 // If we have lots of numbers, set the list to contain a sigle entry.  If we have
@@ -845,9 +861,7 @@ enum {
         [self.numbers replaceObjectsInRange:NSMakeRange(0, [self.numbers count]) withObjectsFromArray:[[self class] defaultNumbers]];
     }
     [self syncLeftBarButtonTitle];
-    if (self.isViewLoaded) {
-        [self.tableView reloadData];
-    }
+
     [self recalculateTotal];
 }
 
